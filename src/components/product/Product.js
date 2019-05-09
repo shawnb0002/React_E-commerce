03adf98b4e128f62d13b5e4db376031d408d308e
Yasean Bastian <shawnb0002@yahoo.com>
2019-05-09 16:45:13 -0400

    getting image data
    getting image data and creating bootstrap cards for the product and setting up the add to cart logic .

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../../context";
 
 class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h3>Hello from Product's</h3>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => console.log("you clicked the image")}
+          >
+            <Link to="../details">
+              <img src={img} alt="Product" className="card-img-top" />
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
+          {/* Card footer */}
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
 
 export default Product;
