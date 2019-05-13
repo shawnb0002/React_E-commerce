3021529c4ced9949a6b34a0afb25f39c4e93be7b
Yasean Bastian <shawnb0002@yahoo.com>
2019-05-13 16:45:45 -0400

    cart button styling

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -75,6 +75,22 @@ const ProductWrapper = styled.div`
   .img-container:hover .card-img-top {
     transform: scale(1.2);
   }
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 1s linear;
+  }
+  .img-container:hover .cart-btn {
+    transform: translate(0%, 0%);
+  }
 `;
 
 export default Product;
